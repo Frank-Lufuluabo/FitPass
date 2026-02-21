2440192d9e33a87ebeee6f896656462da52a7c90
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-02-21 23:17:44 +0200

    feat: update chat module

---
+++ components/app/chat/index.ts
@@ -0,0 +1,6 @@
+export { ChatButton } from "./ChatButton";
+export { MessageBubble } from "./MessageBubble";
+export { WelcomeScreen } from "./WelcomeScreen";
+export { ChatSheet } from "./ChatSheet";
+export { ToolCallUI } from "./ToolCallUI";
+export type { ToolCallPart } from "./types";
