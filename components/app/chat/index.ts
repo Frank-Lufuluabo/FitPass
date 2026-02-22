5d7485ceba0b60b71ff3a24d4d40d53565e5b7d5
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-02-22 23:49:57 +0200

    Remove index.ts file from chat component

--- components/app/chat/index.ts
+++
@@ -1,6 +0,0 @@
-export { ChatButton } from "./ChatButton";
-export { MessageBubble } from "./MessageBubble";
-export { WelcomeScreen } from "./WelcomeScreen";
-export { ChatSheet } from "./ChatSheet";
-export { ToolCallUI } from "./ToolCallUI";
-export type { ToolCallPart } from "./types";
